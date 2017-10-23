99e63968c663f1a518cff04c061e46ee131dcbaa
Ted Regino <tedmacpro2017@Teds-MacBook-Pro.local>
2017-10-23 17:10:34 +1300

    adding new react files to run on local

--- client/client.js
+++ client/client.js
@@ -1,1 +1,10 @@
 
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+
+
+render(
+  <App/>, document.getElementById('app')
+
+)
--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, { Component} from 'react'
 
+class App extends Component{
+
+  render(){
+    return <div> This is defenitely a React App</div>
+
+  }
+
+}
+export  default App
